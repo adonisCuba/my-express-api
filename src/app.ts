3b5158d934168b27c8b019f3017a150d6276c1db
Adonis Gonzalez Santos <adonis.cuba@gmail.com>
2025-01-30 21:19:24 -0500

    Enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { setEmailRoutes } from './routes/emailRoutes';
-require('dotenv').config();
+import express from "express";
+import bodyParser from "body-parser";
+import { setEmailRoutes } from "./routes/emailRoutes";
+import cors from "cors";
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(bodyParser.json());
 
 setEmailRoutes(app);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
